a74780c6fff33efd271039545c14c66f3904d4e5
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-09-28 10:32:51 +0300

    add extra tests

--- spec/QueryExpression.join.spec.js
+++ spec/QueryExpression.join.spec.js
@@ -65,7 +65,7 @@ describe('SqlFormatter', () => {
         }).leftJoin(OrderStatusTypes).with((x, y) => {
             return x.orderStatus === y.id;
         })
-        .where((x) => {
+        .where((x, familyName) => {
             return x.customer.familyName === familyName;
         }, {
             familyName
@@ -73,5 +73,83 @@ describe('SqlFormatter', () => {
         .orderByDescending((x) => x.orderDate).take(25);
         const results = await db.executeAsync(q);
         expect(results.length).toBeTruthy();
+    });
+
+
+    it('should use multiple join statements with parameters', async () => {
+        const Orders = new QueryEntity('OrderData').as('Orders');
+        const Customers = new QueryEntity('PersonData').as('customer');
+        const OrderStatusTypes = new QueryEntity('OrderStatusTypeData').as('orderStatus');
+        const q = new QueryExpression().from(Orders).select((x) => {
+            return {
+                id: x.id,
+                orderedItem: x.orderedItem,
+                customerFamilyName: x.customer.familyName,
+                customerGivenName: x.customer.givenName,
+                orderStatus: x.orderStatus
+            }
+        }).join(OrderStatusTypes).with((x, y, status) => {
+            return x.orderStatus === y.id && x.orderStatus.alternateName === status;
+        }, {
+            status: 'OrderPickup'
+        }).leftJoin(Customers).with((x, y) => {
+            return x.customer === y.id;
+        }).orderByDescending((x) => x.orderDate).take(25);
+        const results = await db.executeAsync(q);
+        expect(results.length).toBeTruthy();
+    });
+
+    it('should use join statement with parameters', async () => {
+        const Orders = new QueryEntity('OrderData').as('Orders');
+        const Products = new QueryEntity('ProductData').as('orderedItem');
+        const q = new QueryExpression().from(Orders).select((x) => {
+            return {
+                id: x.id,
+                orderedItem: x.orderedItem,
+                productName: x.orderedItem.name
+            }
+        }).join(Products).with((x, y) => {
+            return x.orderedItem === y.id
+        })
+        .where((x, product) => {
+            return x.orderedItem.name === product;
+        }, {
+            product: 'MSI GX70 3BE-007US'
+        })
+        .orderByDescending((x) => x.orderDate).take(25);
+        const results = await db.executeAsync(q);
+        expect(results.length).toBeTruthy();
+        results.forEach((result) => {
+            expect(result.productName).toBe('MSI GX70 3BE-007US');
+        })
+    });
+
+    it('should use nested join statement with parameters', async () => {
+        const Orders = new QueryEntity('OrderData').as('Orders');
+        const Customers = new QueryEntity('PersonData').as('customer');
+        const PostalAddresses = new QueryEntity('PostalAddressData').as('address');
+        const q = new QueryExpression().from(Orders).select((x) => {
+            return {
+                id: x.id,
+                orderedItem: x.orderedItem,
+                customerEmail: x.customer.email,
+                customerAddressLocality: x.customer.address.addressLocality
+            }
+        }).join(Customers).with((x, y) => {
+            return x.customer === y.id
+        }).leftJoin(PostalAddresses).with((x, y) => {
+            return x.customer.address === y.id
+        })
+        .where((x, email) => {
+            return x.customer.email === email;
+        }, {
+            email: 'cameron.ball@example.com'
+        })
+        .orderByDescending((x) => x.orderDate).take(25);
+        const results = await db.executeAsync(q);
+        expect(results.length).toBeTruthy();
+        results.forEach((result) => {
+            expect(result.customerEmail).toBe('cameron.ball@example.com');
+        })
     });
 });
