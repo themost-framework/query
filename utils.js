7514b07cf5a84711ceda3d04262234a8410d4020
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-12-05 07:47:00 +0000

    update utils module

--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
 // MOST Web Framework 2.0 Codename Blueshift Copyright (c) 2017-2020, THEMOST LP All rights reserved
-var QueryExpression = require('./query').QueryExpression;
+const {QueryExpression} = require('./query');
 
 /**
  * @param {string} tz
  */
 function convertTimezone(tz) {
     if (tz === "Z") return 0;
-    var m = tz.match(/([+\-\s])(\d\d):?(\d\d)?/);
+    let m = tz.match(/([+\-\s])(\d\d):?(\d\d)?/);
     if (m) {
         return (m[1] === '-' ? -1 : 1) * (parseInt(m[2], 10) + ((m[3] ? parseInt(m[3], 10) : 0) / 60)) * 60;
     }
@@ -22,10 +22,10 @@ function zeroPad(number, length) {
 }
 
 function dateToString(date, timeZone) {
-    var dt = new Date(date);
+    let dt = new Date(date);
 
     if (timeZone !== 'local') {
-        var tz = convertTimezone(timeZone);
+        let tz = convertTimezone(timeZone);
 
         dt.setTime(dt.getTime() + (dt.getTimezoneOffset() * 60000));
         if (tz !== false) {
@@ -33,13 +33,13 @@ function dateToString(date, timeZone) {
         }
     }
 
-    var year   = dt.getFullYear();
-    var month  = zeroPad(dt.getMonth() + 1, 2);
-    var day    = zeroPad(dt.getDate(), 2);
-    var hour   = zeroPad(dt.getHours(), 2);
-    var minute = zeroPad(dt.getMinutes(), 2);
-    var second = zeroPad(dt.getSeconds(), 2);
-    var millisecond = zeroPad(dt.getMilliseconds(), 3);
+    let year   = dt.getFullYear();
+    let month  = zeroPad(dt.getMonth() + 1, 2);
+    let day    = zeroPad(dt.getDate(), 2);
+    let hour   = zeroPad(dt.getHours(), 2);
+    let minute = zeroPad(dt.getMinutes(), 2);
+    let second = zeroPad(dt.getSeconds(), 2);
+    let millisecond = zeroPad(dt.getMilliseconds(), 3);
 
     return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + '.' + millisecond;
 }
@@ -49,13 +49,13 @@ function dateToString(date, timeZone) {
  * @returns {string}
  */
 function bufferToString(buffer) {
-    var hex = '';
+    let hex = '';
     try {
         hex = buffer.toString('hex');
     } catch (err) {
         // node v0.4.x does not support hex / throws unknown encoding error
-        for (var i = 0; i < buffer.length; i++) {
-            var byte = buffer[i];
+        for (let i = 0; i < buffer.length; i++) {
+            let byte = buffer[i];
             // noinspection JSCheckFunctionSignatures
             hex += zeroPad(byte.toString(16));
         }
@@ -65,10 +65,10 @@ function bufferToString(buffer) {
 }
 
 function objectToValues(object, timeZone) {
-    var values = [];
-    for (var key in object) {
+    let values = [];
+    for (let key in object) {
         if (Object.prototype.hasOwnProperty.call(object, key)) {
-            var value = object[key];
+            let value = object[key];
             if(typeof value === 'function') {
                 continue;
             }
@@ -98,7 +98,7 @@ function escapeId(val, forbidQualified) {
     return '`' + val.replace(/`/g, '``').replace(/\./g, '`.`') + '`';
 }
 // eslint-disable-next-line no-control-regex
-var STR_ESCAPE_REGEXP = /[\0\n\r\b\t\\'"\x1a]/g;
+let STR_ESCAPE_REGEXP = /[\0\n\r\b\t\\'"\x1a]/g;
 
 function escape(val, stringifyObjects, timeZone) {
     if (typeof val === 'undefined' || val === null) {
@@ -145,108 +145,106 @@ function escape(val, stringifyObjects, timeZone) {
 
 function format(sql, values, stringifyObjects, timeZone) {
     values = (typeof values === 'undefined' || values === null) ? [] : [].concat(values);
-    var index = 0;
+    let index = 0;
     return sql.replace(/\?\??/g, function(match) {
         if (index === values.length) {
             return match;
         }
-        var value = values[index++];
+        let value = values[index++];
         return match === '??'
             ? escapeId(value)
             : escape(value, stringifyObjects, timeZone);
     });
 }
 
-/**
- * @class
- * @constructor
- */
-function QueryUtils() {
+class QueryUtils {
+    constructor() {
+    }
 
+    /**
+     * Initializes a select query expression by specifying the entity name
+     * @param {string|*} entity - The name of the entity
+     */
+    static query(entity) {
+        let q = new QueryExpression();
+        return q.from(entity);
+    }
+
+    /**
+     * Initializes a select query expression
+     * @param {...*} fields
+     */
+    static select(fields) {
+        let q = new QueryExpression();
+        return q.select.apply(q, fields);
+    }
+
+    /**
+     * Initializes an insert query expression
+     * @param {*} obj - The object to insert
+     */
+    static insert(obj) {
+        let q = new QueryExpression();
+        return q.insert(obj);
+    }
+
+    /**
+     * Initializes an update query expression
+     * @param {string|*} entity - The name of the entity
+     */
+    static update(entity) {
+        let q = new QueryExpression();
+        return q.update(entity);
+    }
+
+    /**
+     * Initializes a delete query expression
+     * @param {string} entity - The name of the entity
+     */
+    static delete(entity) {
+        let q = new QueryExpression();
+        return q.delete(entity);
+    }
+    
+    /**
+     * Initializes a QueryExpression instance.
+     * @returns {QueryExpression}
+     * @param  {*} obj
+     */
+    static where(obj) {
+        let q = new QueryExpression();
+        return q.where(obj);
+    }
 }
 
-/**
- * Initializes a select query expression by specifying the entity name
- * @param {string|*} entity - The name of the entity
- */
-QueryUtils.query = function(entity) {
-    var q = new QueryExpression();
-    return q.from(entity);
-};
-/**
- * Initializes a select query expression
- * @param {...*} fields
- */
-QueryUtils.select = function(fields) {
-    var q = new QueryExpression();
-    return q.select.apply(q,fields);
-};
-/**
- * Initializes an insert query expression
- * @param {*} obj - The object to insert
- */
-QueryUtils.insert = function(obj) {
-    var q = new QueryExpression();
-    return q.insert(obj);
-};
+class SqlUtils {
+    constructor() {
+    }
 
-/**
- * Initializes an update query expression
- * @param {string|*} entity - The name of the entity
- */
-QueryUtils.update = function(entity) {
-    var q = new QueryExpression();
-    return q.update(entity);
-};
+    /**
+     * Escapes the given value and returns an equivalent string which is going to be used in SQL expressions.
+     * @param {*} val
+     * @returns {*}
+     */
+    static escape(val) {
+        return escape(val);
+    }
 
-/**
- * Initializes a delete query expression
- * @param {string} entity - The name of the entity
- */
-QueryUtils.delete = function(entity) {
-    var q = new QueryExpression();
-    return q.delete(entity);
-};
-
-/**
- * Initializes a QueryExpression instance.
- * @returns {QueryExpression}
- * @param  {*} obj
- */
-QueryUtils.where = function(obj) {
-    var q = new QueryExpression();
-    return q.where(obj);
-};
-
-/**
- * @class
- * @constructor
- */
-function SqlUtils() {
-
+    /**
+     * Formats the given SQL expression string and replaces parameters with the given parameters, if any.
+     * e.g. * SELECT * FROM User where username=? with values: ['user1'] etc.
+     * @param {string} sql
+     * @param {*=} values
+     * @returns {*}
+     */
+    static format(sql, values) {
+        return format(sql, values);
+    }
 }
-/**
- * Escapes the given value and returns an equivalent string which is going to be used in SQL expressions.
- * @param {*} val
- * @returns {*}
- */
-SqlUtils.escape = function(val) {
-    return escape(val);
+
+
+
+module.exports = {
+    QueryUtils,
+    SqlUtils
 };
-
-/**
- * Formats the given SQL expression string and replaces parameters with the given parameters, if any.
- * e.g. * SELECT * FROM User where username=? with values: ['user1'] etc.
- * @param {string} sql
- * @param {*=} values
- * @returns {*}
- */
-SqlUtils.format = function(sql, values) {
-    return format(sql, values);
-};
-
-
-if (typeof exports !== 'undefined') {
-    module.exports.QueryUtils = QueryUtils;
-    module.exports.SqlUtils = SqlUtils;
-}
