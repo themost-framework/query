0f5405355aa5005cc942217a005a719d8c45c07f
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-12-05 11:16:21 +0000

    export classes

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 // MOST Web Framework 2.0 Codename Blueshift Copyright (c) 2017-2021, THEMOST LP All rights reserved
-const _formatter = require('./formatter');
-const _odata = require('./odata');
-const _expressions = require('./expressions');
-const _query = require('./query');
-const _utils = require('./utils');
+const {SqlFormatter} = require('./formatter');
+const {Token, LiteralToken, SyntaxToken,
+    IdentifierToken, OpenDataParser} = require('./odata');
+const {LogicalExpression, MethodCallExpression, LiteralExpression,
+    MemberExpression, ArithmeticExpression, ComparisonExpression,
+    Operators} = require('./expressions');
+const {QueryField, QueryEntity, QueryExpression} = require('./query');
+const {SqlUtils, QueryUtils} = require('./utils');
 
-module.exports.SqlFormatter = _formatter.SqlFormatter;
-
-module.exports.Token = _odata.Token;
-module.exports.LiteralToken = _odata.LiteralToken;
-module.exports.SyntaxToken = _odata.SyntaxToken;
-module.exports.IdentifierToken = _odata.IdentifierToken;
-module.exports.OpenDataParser = _odata.OpenDataParser;
-
-module.exports.LogicalExpression = _expressions.LogicalExpression;
-module.exports.MethodCallExpression = _expressions.MethodCallExpression;
-module.exports.LiteralExpression = _expressions.LiteralExpression;
-module.exports.MemberExpression = _expressions.MemberExpression;
-module.exports.ArithmeticExpression = _expressions.ArithmeticExpression;
-module.exports.ComparisonExpression = _expressions.ComparisonExpression;
-module.exports.Operators = _expressions.Operators;
-
-module.exports.QueryField = _query.QueryField;
-module.exports.QueryEntity = _query.QueryEntity;
-module.exports.QueryExpression = _query.QueryExpression;
-
-module.exports.QueryUtils = _utils.QueryUtils;
-module.exports.SqlUtils = _utils.SqlUtils;
+module.exports = {
+    Operators,
+    SqlFormatter,
+    Token,
+    LiteralToken,
+    SyntaxToken,
+    IdentifierToken,
+    OpenDataParser,
+    QueryField,
+    QueryEntity,
+    QueryExpression,
+    SqlUtils,
+    QueryUtils,
+    LogicalExpression,
+    MethodCallExpression,
+    LiteralExpression,
+    MemberExpression,
+    ArithmeticExpression,
+    ComparisonExpression
+}
