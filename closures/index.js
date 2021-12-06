a1147eab564435fd74a26c423f3506cba9084711
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-12-06 05:24:23 +0000

    update closure exports

--- closures/index.js
+++ closures/index.js
@@ -1,18 +1,23 @@
 // MOST Web Framework 2.0 Codename Blueshift Copyright (c) 2017-2020, THEMOST LP All rights reserved
-var _closureParser = require('./ClosureParser');
-var PrototypeMethodParser = require('./PrototypeMethodParser').PrototypeMethodParser;
-var DateMethodParser = require('./DateMethodParser').DateMethodParser;
-var StringMethodParser = require('./StringMethodParser').StringMethodParser;
-var MathMethodParser = require('./MathMethodParser').MathMethodParser;
+const { count, round, min, max, sum,
+    mean, avg, length, ClosureParser} = require('./ClosureParser');
+const {PrototypeMethodParser} = require('./PrototypeMethodParser');
+const {DateMethodParser} = require('./DateMethodParser');
+const {StringMethodParser} = require('./StringMethodParser');
+const {MathMethodParser} = require('./MathMethodParser');
 
-module.exports.ClosureParser = _closureParser.ClosureParser;
-module.exports.count = _closureParser.count;
-module.exports.min = _closureParser.min;
-module.exports.max = _closureParser.max;
-module.exports.mean = _closureParser.mean;
-module.exports.round = _closureParser.round;
-module.exports.sum = _closureParser.sum;
-module.exports.PrototypeMethodParser = PrototypeMethodParser;
-module.exports.DateMethodParser = DateMethodParser;
-module.exports.StringMethodParser = StringMethodParser;
-module.exports.MathMethodParser = MathMethodParser;
+module.exports = {
+    count,
+    round,
+    min,
+    max,
+    sum,
+    mean,
+    avg,
+    length,
+    ClosureParser,
+    PrototypeMethodParser,
+    DateMethodParser,
+    StringMethodParser,
+    MathMethodParser
+}
