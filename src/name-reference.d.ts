5423406e053ecd8c9ee80e1ba9f433d5428a336a
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-10-18 07:27:33 +0000

    add type declarations

---
+++ src/name-reference.d.ts
@@ -0,0 +1,6 @@
+
+export declare function isMethodOrNameReference(str: string): string;
+
+export declare function isNameReference(str: string): string;
+
+export declare function trimNameReference(str: string): string;
