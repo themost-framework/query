a5d632e0b8a5d7e776a526cf577856625cac48ec
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2026-05-25 13:59:41 +0300

    implement sql synonyms

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,14 @@
 // MOST Web Framework Codename Blueshift Copyright (c) 2017-2022, THEMOST LP All rights reserved
 
-export * from "./formatter";
-export * from "./odata";
-export * from "./expressions";
-export * from "./query";
-export * from "./utils";
-export * from "./object-name.validator";
-export * from "./simple-open-data-parser";
-export * from "./open-data-query.formatter";
-export * from "./open-data-query.expression";
-export * from "./closures";
-export * from "./name-reference";
+export * from './formatter';
+export * from './sql-synonym';
+export * from './odata';
+export * from './expressions';
+export * from './query';
+export * from './utils';
+export * from './object-name.validator';
+export * from './simple-open-data-parser';
+export * from './open-data-query.formatter';
+export * from './open-data-query.expression';
+export * from './closures';
+export * from './name-reference';
